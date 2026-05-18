16b7136fea26e13314a1415a9a3f307db8ba88e9
ddtechsofficial <ddtechsofficial@gmail.com>
2026-05-18 17:12:14 +0530

    update constant file and remove port variable

--- server/constant.py
+++ server/constant.py
@@ -6,8 +6,8 @@
 
 import os
 
-DBHOST = os.environ.get("MYSQL_HOST", "localhost")
-DBUSER = os.environ.get("MYSQL_USER", "root")
-DBPASSWORD = os.environ.get("MYSQL_PASSWORD", "Password@123")
-DBNAME = os.environ.get("MYSQL_DATABASE", "crud_app")
-DBPORT = int(os.environ.get("MYSQLPORT", 3306))
+DBHOST = os.environ.get("MYSQLHOST", "localhost")
+DBUSER = os.environ.get("MYSQLUSER", "root")
+DBPASSWORD = os.environ.get("MYSQLPASSWORD", "Password@123")
+DBNAME = os.environ.get("MYSQLDATABASE", "crud_app")
+# DBPORT = int(os.environ.get("MYSQLPORT", 3306))
