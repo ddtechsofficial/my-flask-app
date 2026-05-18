01c94897f276b343a770b72d44f63f569c2e449f
ddtechsofficial <ddtechsofficial@gmail.com>
2026-05-18 16:49:25 +0530

    DB creds

--- server/constant.py
+++ server/constant.py
@@ -2,3 +2,11 @@ DBNAME="crud_app"
 DBHOST="localhost"
 DBUSER="root"
 DBPASSWORD="Password@123"
+
+
+import os
+
+DBHOST = os.environ.get("MYSQL_HOST", "localhost")
+DBUSER = os.environ.get("MYSQL_USER", "root")
+DBPASSWORD = os.environ.get("MYSQL_PASSWORD", "Password@123")
+DBNAME = os.environ.get("MYSQL_DATABASE", "crud_app")
