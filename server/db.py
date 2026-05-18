d1473caf4396aa80c5b59218e5fc5b793c6ca4f7
ddtechsofficial <ddtechsofficial@gmail.com>
2026-05-18 17:04:08 +0530

    fix db connection for railway

--- server/db.py
+++ server/db.py
@@ -1,12 +1,13 @@
 from mysql.connector import connect
-from server.constant import DBHOST, DBNAME, DBPASSWORD, DBUSER
+from server.constant import DBHOST, DBNAME, DBPASSWORD, DBUSER, DBPORT
 def db_connect():
     try:
         return connect(
             host=DBHOST,
             database=DBNAME,
             user=DBUSER,
-            password=DBPASSWORD
+            password=DBPASSWORD,
+            port=DBPORT
         )
     except Exception as e:
         print("Error while connecting to DB: ", e)
